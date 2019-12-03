891557ca2915e66c305e4648cfa0ec227efcc093
Elom Amouzou <elom1990@yahoo.com>
2019-12-03 09:01:05 -0700

    Created ship instance variable

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,11 +6,22 @@ require './lib/cell'
 class CellTest < Minitest::Test
 
   def setup
+    @cell = Cell.new("B4")
   end
 
   def test_cell_exists
-    cell = Cell.new("B4")
 
-    assert_instance_of Cell, cell
+    assert_instance_of Cell, @cell
   end
+
+  def test_cell_coordinate
+
+    assert_equal "B4", @cell.coordinate
+  end
+
+  def test_cell_ship
+
+    assert_equal nil, @cell.ship
+  end
+
 end
