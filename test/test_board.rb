0feeab7e202efb4f5545a3cd61554fee72db66a2
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-03 22:13:50 -0700

    Write pseudocode for adjacent cell check

--- test/test_board.rb
+++ test/test_board.rb
@@ -34,11 +34,23 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate?("A22")
   end
 
-  def test_valid_placement
+  def test_valid_placement_length
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     assert @board.valid_placement?(cruiser, ["A1", "A2", "A3"])
     refute @ibboard.valid_placement?(submarine, ["A1", "A2", "A3"])
+  end
+
+  def test_valid_placement_consecutive
+#this one will be difficult
+#my idea is to grab the proposed array
+#split the cell coordinate into letter and number
+#then change the letter into its associated ASCII number
+#using this, you have to compare consecutive cells in the proposed valid_placement array and make sure that the letter is the same AND the number is one away OR the letter is one away from the next AND that the number is the same
+
+#ANOTHER OPTION
+
+#come up with the 24 associations, ie A1 and A2, B3 and C3, D3 and D4 
   end
 end
